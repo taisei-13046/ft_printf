66379bbae99a1067d2b3cd680bfc41874a70d30a
taisei-13046 <78260526+taisei-13046@users.noreply.github.com>
2021-05-21 13:12:28 +0900

    Update input.c

--- input.c
+++ input.c
@@ -7,17 +7,17 @@ int	per_print(const char **format, va_list *ap, t_flag *ans)
 	return_num = 0;
 	if (ans->specific == -1)
 		return (-1);
-	if (ans->specific == 0)
+	if (ans->specific == CHAR_SPEC)
 		char_print(ap, ans, &return_num);
-	else if (ans->specific == 1)
+	else if (ans->specific == STRING_SPEC)
 		string_print(ap, ans, &return_num);
-	else if (ans->specific == 2)
+	else if (ans->specific == ADRESS_SPEC)
 		addres_print(ap, ans, &return_num);
-	else if (ans->specific == 3 || ans->specific == 4)
+	else if (ans->specific == INT_D_SPEC || ans->specific == INT_I_SPEC)
 		int_print(ap, ans, &return_num);
-	else if (ans->specific == 5)
+	else if (ans->specific == UN_INT_SPEC)
 		unsigned_int_print(ap, ans, &return_num);
-	else if (ans->specific == 6 || ans->specific == 7)
+	else if (ans->specific == SX_SPEC || ans->specific == LX_SPEC)
 		unsigned_print(ap, ans, &return_num);
 	else
 		percent_print(ans, &return_num);
@@ -55,37 +55,10 @@ void	reset_flag(t_flag *ans)
 {
 	ans->flag[0] = 0;
 	ans->flag[1] = 0;
-	ans->field = -1;
-	ans->acc = -1;
-	ans->specific = -1;
+	ans->field = NONE;
+	ans->acc = NONE;
+	ans->specific = NONE;
 	ans->putlen = 0;
-}
-
-void	write_percent(const char **format, int *return_num, va_list *ap)
-{
-	int		num;
-	t_flag	ans;
-
-	num = 0;
-	(*format)++;
-	reset_flag(&ans);
-	while (num >= 0)
-	{
-		num = ft_checkflag("0-", **format);
-		if (num == -1)
-			break ;
-		ans.flag[num] = 1;
-		(*format)++;
-	}
-	ans.field = check_field_acc(format, ap, 0, &ans);
-	if (**format == '.')
-	{
-		(*format)++;
-		ans.acc = check_field_acc(format, ap, 1, &ans);
-	}
-	ans.specific = ft_checkflag("cspdiuxX%", **format);
-	num = per_print(format, ap, &ans);
-	*return_num += num;
 }
 
 void	ft_print_str(const char **format, int *return_num)
@@ -99,4 +72,31 @@ void	ft_print_str(const char **format, int *return_num)
 	len = *format - str;
 	write(1, str, len);
 	*return_num += len;
+}
+
+void	write_percent(const char **format, int *return_num, va_list *ap)
+{
+	int		num;
+	t_flag	ans;
+
+	num = 0;
+	(*format)++;
+	reset_flag(&ans);
+	while (num >= 0)
+	{
+		num = ft_checkflag("0-", **format);
+		if (num == NONE)
+			break ;
+		ans.flag[num] = 1;
+		(*format)++;
+	}
+	ans.field = check_field_acc(format, ap, 0, &ans);
+	if (**format == '.')
+	{
+		(*format)++;
+		ans.acc = check_field_acc(format, ap, 1, &ans);
+	}
+	ans.specific = ft_checkflag("cspdiuxX%", **format);
+	num = per_print(format, ap, &ans);
+	*return_num += num;
 }
